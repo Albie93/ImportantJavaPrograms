c33f094655087f39778c48ff0f727343e4e24c03
Albie93 <albiestar779@gmail.com>
2017-11-28 00:19:13 +0530

    Third Commit for Modification with  FactorialWithIteration

--- src/basic/programs/FactorailWithIteration.java
+++ src/basic/programs/FactorailWithIteration.java
@@ -3,7 +3,8 @@ package basic.programs;
 import java.util.Scanner;
 
 public class FactorailWithIteration {
-
+    //Write a code For FactorialWithIteration
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Eneter The Nuumber:");
